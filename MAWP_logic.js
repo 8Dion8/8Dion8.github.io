8a907d036ff0a2d0aa2161e7fa3977a2854489f9
8Dion8 <shvartserinfo@gmail.com>
2020-07-23 12:23:31 +0300

    upd

--- MAWP_logic.js
+++ MAWP_logic.js
@@ -131,6 +131,8 @@ function run_code() {
             for (let i = 0; i < input.length; ++i) {
                 stack.push(input[i].charCodeAt(0))
             }
+        } else if (char == '~') {
+            stack = stack.reverse()
         }
         pos += 1
             //console.log(stack)
@@ -146,3 +148,4 @@ function run_code() {
         }
     }
 }
+//https://8dion8.github.io/?code=%5B!!3P3WA%3C75W2W%3B73W5W%3B65W2W1M2W%3B65W2W1M2W%3B%3E%25!!5P5WA%3C92M6W%3B94M9W%3B65W2W1M2W%3B65W2W1M2W%3B%3E%25!55WA%3F.%251M25W%3B%5D&input=%23Fizz%2075W2W%3B73W5W%3B65W2W1M2W%3B65W2W1M2W%3B%20%23Buzz%2092M6W%3B94M9W%3B65W2W1M2W%3B65W2W1M2W%3B
