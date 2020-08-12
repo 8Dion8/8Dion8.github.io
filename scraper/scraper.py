91df7e91acd6c5240161c8b4764d7eabd97168ec
8Dion8 <shvartserinfo@gmail.com>
2020-08-12 11:27:04 +0300

    Update post info

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -16,13 +16,14 @@ def git_push():
     except:
         print('Some error occured while pushing the code')
 
-link = 'https://codegolf.stackexchange.com/search?tab=active&q="MAWP%2c%20"%20is%3aanswer'
+link = 'https://codegolf.stackexchange.com/search?tab=Active&pagesize=9999&q=%22MAWP%2c%20%22%20is%3aanswer'
 titles = []
 links = []
 f = requests.get(link)
 soup = BeautifulSoup(f.text, 'html.parser')
 results = soup.find_all(class_="result-link")
 for i in results:
+    print(i)
     preformatted = i.text
     formatted = preformatted.replace('\n\n\r\n            A: ','')
     formatted = formatted.replace('\r\n        \n\n','')
