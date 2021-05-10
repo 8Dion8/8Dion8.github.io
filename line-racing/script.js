54f83c1f45a700a2d620b52e4955a0099dc1b59f
8Dion8 <shvartserinfo@gmail.com>
2021-05-10 09:19:35 +0300

    Added resizing and better grid generation

--- line-racing/script.js
+++ line-racing/script.js
@@ -1,50 +1,61 @@
-var cell_size = 30;
-var width = 1080 / cell_size;
-var height = 720 / cell_size;
+/// < reference path="./p5.global-mode.d.ts" / >
+
+var cell_size;
+var gridWidth = 32;
+var gridHeight = 20;
 
 function Goal(x, y, direction, length, color) {
-  this.x0 = x;
-  this.y0 = y;
-  if (direction == "Vertical") {
-    this.x1 = x;
-    this.y1 = y + length;
-  } else if (direction == "Horizontal") {
-    this.x1 = x + length;
-    this.y1 = y;
-  }
+    this.x0 = x;
+    this.y0 = y;
+    if (direction == "Vertical") {
+        this.x1 = x;
+        this.y1 = y + length;
+    } else if (direction == "Horizontal") {
+        this.x1 = x + length;
+        this.y1 = y;
+    }
 
-  this.draw = function () {
-    stroke(color);
-    strokeWeight(3);
-    line(
-      this.x0 * cell_size + 1,
-      this.y0 * cell_size + 1,
-      this.x1 * cell_size + 1,
-      this.y1 * cell_size + 1
-    );
-  };
+    this.draw = function() {
+        stroke(color);
+        strokeWeight(cell_size / 10);
+        line(
+            this.x0 * cell_size + 1,
+            this.y0 * cell_size + 1,
+            this.x1 * cell_size + 1,
+            this.y1 * cell_size + 1
+        );
+    };
 }
 
 function drawGrid() {
-  strokeWeight(2);
-  stroke([82, 130, 214]);
-  for (var i = 1; i < width - 1; i++) {
-    line(i * cell_size + 1, 0, i * cell_size + 1, height * cell_size);
-  }
-  for (var i = 1; i < height - 1; i++) {
-    line(0, i * cell_size + 1, width * cell_size, i * cell_size);
-  }
+    strokeWeight(cell_size / 15);
+    stroke([82, 130, 214]);
+    for (var i = 1; i < gridWidth; i++) {
+        line(i * cell_size + 1, 0, i * cell_size + 1, gridHeight * cell_size);
+    }
+    for (var i = 1; i < gridHeight; i++) {
+        line(0, i * cell_size + 1, gridWidth * cell_size, i * cell_size);
+    }
 }
 
 var start = new Goal(2, 1, "Vertical", 6, [10, 240, 30]);
 var end = new Goal(10, 5, "Horizontal", 6, [240, 10, 10]);
+var Для_Ксю = new Goal(20, 10, "Horizontal", 10, [139, 0, 139]);
 
 function setup() {
-  createCanvas(1080, 720);
+    cell_size = Math.min(windowWidth / gridWidth, windowHeight / gridHeight);
+    createCanvas(gridWidth * cell_size, gridHeight * cell_size);
 }
+
 function draw() {
-  background([242, 238, 203]);
-  drawGrid();
-  start.draw();
-  end.draw();
+    background([242, 238, 203]);
+    drawGrid();
+    start.draw();
+    end.draw();
+    Для_Ксю.draw();
+}
+
+function windowResized() {
+    cell_size = Math.min(windowWidth / gridWidth, windowHeight / gridHeight);
+    resizeCanvas(gridWidth * cell_size, gridHeight * cell_size);
 }
