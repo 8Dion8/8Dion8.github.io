ae1f87eb7b19ae3614cbea97d3c2bb4d37a4dd24
8Dion8 <shvartserinfo@gmail.com>
2021-06-22 12:07:39 +0300

    huyak huyak

--- secret/script.js
+++ secret/script.js
@@ -1,6 +1,14 @@
 function magic() {
     var img = document.createElement("img");
     img.src = "acura.png";
-    var src = document.body;
+    var src = document.getElementsByClassName("center")[0];
     src.appendChild(img);
+    var button = document.getElementById("button");
+    button.remove();
+    var txt1 = document.createElement("h2");
+    txt1.innerHTML = "Now you can have your car everywhere you go :))";
+    var txt2 = document.createElement("h2");
+    txt2.innerHTML = "🥳Happy Birthday!🥳";
+    src.appendChild(txt1);
+    src.appendChild(txt2);
 }
