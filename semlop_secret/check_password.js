2a34a40a7c05464f24642ed27c6796236e639051
8Dion8 <shvartserinfo@gmail.com>
2021-08-02 22:47:29 +0300

    lol dont matter dont look

---
+++ semlop_secret/check_password.js
@@ -0,0 +1,80 @@
+function hash(str) {
+    return str.split('').reduce((prevHash, currVal) =>
+        (((prevHash << 5) - prevHash) + currVal.charCodeAt(0)) | 0, 0);
+}
+
+function checkPassword() {
+    var inputted = document.getElementById("secret").value + '';
+    var test_hash = hash("31.01.21");
+    console.log(test_hash, hash(inputted), inputted);
+    if (hash(inputted) == test_hash) {
+        document.open();
+        document.write(
+            `
+            <!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="stylesheet" href="style.css">
+                </link>
+                <title>С днём рождения!</title>
+            </head>
+
+            <body>
+                <div id="main_cont">
+                    <h2>У тебя получилось :)</h2>
+                    <h3>(нифига ты умный)</h3>
+                    <h1>С днём рождения!</h1>
+                    <p>Мы тут решили тебе написать пару теплых слов. <br>
+                        Ну так, чтобы день рождения хоть как то отметить, <br>
+                        а то что значит ничо делать не будешь!<br>
+                        P.S. нажми на картинку</p>
+                </div>
+                <div id="img_cont">
+                    <a href="Письмо.pdf" download="Письмо.pdf">
+                        <img src="birth2.jpg" width="100%" height="100%">
+                    </a>
+                </div>
+                <p>Ну а я просто пожелаю чтобы жить было в кайф - в этом собственно и смысл, так же? <br>
+                    Когда мы умрем ничего же с собой взять не получится, так что самое важное - то, как мы прожили)) <br>
+                    С днём рождения)</p>
+            </body>
+
+            </html>
+            `
+        );
+        document.close();
+    } else {
+        document.open();
+        document.write(
+            `
+            <!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="style.css">
+                </link>
+                <title>:(</title>
+            </head>
+
+            <body>
+                <h1>У тя не получилось( как так то(</h1>
+                <div class="container">
+                    <div class="center">
+                        <a href="/index.html">Тыкни сюда чтобы перезагрузить</a>
+                    </div>
+                </div>
+            </body>
+
+            </html>
+            `
+        );
+        document.close();
+    }
+}
