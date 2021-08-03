c6740ebc74612469eac078217bd10a9e2ba16bd4
8Dion8 <shvartserinfo@gmail.com>
2021-08-03 19:32:58 +0300

    complex heightmaps

--- islands/script.js
+++ islands/script.js
@@ -11,80 +11,125 @@ $(document).ready(function() {
 console.log(ip);//*/
 
 
-function generate_seed_from_string(string) {
-    let seed = "";
-    for (let i of string) {
-        seed += i.charCodeAt(0);
-    }
-    return parseInt(seed);
+function hashCode(str) {
+    return str.split('').reduce((prevHash, currVal) =>
+        (((prevHash << 5) - prevHash) + currVal.charCodeAt(0)) | 0, 0);
 }
 
-console.log(generate_seed_from_string("test123"))
-var canvasWidth =  100;
-var canvasHeight = 100;
-var cellWidth = 500 / canvasWidth;
-var cellHeight = 500 / canvasHeight;
-var seed = generate_seed_from_string(document.getElementById("seed").value)
+
+//from https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function componentToHex(c) {
+    let hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+function hex_to_rgb(hex) {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    let r = result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+    return [r.r, r.g, r.b];
+}
+
+
+function get_color_shade(color1, color2, minVal, maxVal, point, minPoint = 0) {
+    if (minPoint && point <= minPoint) {point = minPoint}
+    let r_step = Math.abs(color1[0] - color2[0]) / (maxVal - minVal);
+    let g_step = Math.abs(color1[1] - color2[1]) / (maxVal - minVal);
+    let b_step = Math.abs(color1[2] - color2[2]) / (maxVal - minVal);
+    let r = Math.floor(r_step * point + color1[0]);
+    let g = Math.floor(g_step * point + color1[1]);
+    let b = Math.floor(b_step * point + color1[2]);
+    return rgbToHex(r, g, b);
+}
+
+
+console.log(hashCode("test123"))
+var canvasResolution = document.getElementById("canvasResolution").value;
+var cellSize = 840 / canvasResolution
+var seed = hashCode(document.getElementById("seed").value)
 console.log(seed)
 noise.seed(seed);
 var canvas = document.getElementById("mainCanvas");
 var ctx = canvas.getContext("2d");
-var threshold = document.getElementById("threshold").value;
+var oceanThreshold = document.getElementById("oceanThreshold").value;
+var beachThreshold = document.getElementById("beachThreshold").value;
+var grassThreshold = document.getElementById("grassThreshold").value;
 var current_stage = 0;
 
 ctx.fillStyle = "#000000"
-ctx.fillRect(0, 0, 500, 500);
+ctx.fillRect(0, 0, 840, 840);
 var noiseArr = [];
 
 function update_everything() {
+    canvasResolution = document.getElementById("canvasResolution").value;
+    cellSize = 840 / canvasResolution
     //console.log("updating")
-    seed = generate_seed_from_string(document.getElementById("seed").value)
+    seed = hashCode(document.getElementById("seed").value)
     noise.seed(seed);
-    threshold = document.getElementById("threshold").value;
+    oceanThreshold = document.getElementById("oceanThreshold").value;
+    beachThreshold = document.getElementById("beachThreshold").value;
+    grassThreshold = document.getElementById("grassThreshold").value;
     if (current_stage > 0) {
         generate_noise(false);
     }
     if (current_stage > 1) {
-        make_noise_bw(false);
+        threshold_noise(false);
     }
 }
 
-function make_noise_bw(change_stage=true) {
-    if (change_stage) {current_stage = 2;}
-    for (var x = 0; x < canvasWidth; x++) {
-        for (var y = 0; y < canvasHeight; y++) {
+function threshold_noise(change_stage = true) {
+    if (change_stage) { current_stage = 2; }
+    for (var x = 0; x < canvasResolution; x++) {
+        for (var y = 0; y < canvasResolution; y++) {
             var val = noiseArr[x][y];
-            if (val > threshold) {
+            if (val < oceanThreshold) {
+                noiseArr[x][y] = val;
+                ctx.fillStyle = get_color_shade(
+                    hex_to_rgb("#125e8a"),
+                    hex_to_rgb("#5adbff"),
+                    0,
+                    oceanThreshold,
+                    val,
+                    35,
+                    ""
+                );
+            } else if (val < beachThreshold) {
+                noiseArr[x][y] = beachThreshold - 1;
+                ctx.fillStyle = "#ffdd4a";
+            } else if (val < grassThreshold) {
+                noiseArr[x][y] = grassThreshold - 1;
+                ctx.fillStyle = "#00cc74";
+            } else {
                 noiseArr[x][y] = 255;
-                ctx.fillStyle = "#ffffff";
-                ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+                ctx.fillStyle = "#99989a";
             }
-            else {
-                noiseArr[x][y] = 0;
-                ctx.fillStyle = "#000000";
-                ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
-            }
+            ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
         }
     }
 }
 
 
 
-function generate_noise(change_stage=true) {
-    if (change_stage) {current_stage = 1;}
-    for (let i = 0; i < canvasWidth; i++) {
-        noiseArr.push(new Array(cellHeight));
+function generate_noise(change_stage = true) {
+    if (change_stage) { current_stage = 1; }
+    for (let i = 0; i < canvasResolution; i++) {
+        noiseArr.push(new Array(cellSize));
     }
-    for (var x = 0; x < canvasWidth; x++) {
-        for (var y = 0; y < canvasHeight; y++) {
-            var val = Math.floor(Math.abs(noise.perlin2(x / canvasWidth * 3, y / canvasHeight * 3)) * 255);
+    for (var x = 0; x < canvasResolution; x++) {
+        for (var y = 0; y < canvasResolution; y++) {
+            var val = Math.floor(Math.abs(noise.perlin2(x / canvasResolution * 3, y / canvasResolution * 3)) * 255);
             noiseArr[x][y] = val;
             //console.log(val);
             //if (val > 255) {console.log(val)}
-            ctx.fillStyle = "rgb("+val+","+val+","+val+")";
+            ctx.fillStyle = "rgb(" + val + "," + val + "," + val + ")";
             //console.log("rgb("+val+","+val+","+val+")");
 
-            ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+            ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
         }
     }
 }
